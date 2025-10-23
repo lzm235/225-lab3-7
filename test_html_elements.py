2529460b7212f046f5c6e77c25a9d83a11065c3a
lzm235 <liz227@miamioh.edu>
2025-10-22 23:02:26 -0400

    Refactor test setup and improve h5 tag check

--- test_html_elements.py
+++ test_html_elements.py
@@ -1,27 +1,27 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import unittest
 
 class TestH5Tag(unittest.TestCase):
     def setUp(self):
-        # Setup Firefox options
-        firefox_options = Options()
-        firefox_options.add_argument("--headless")  # Ensures the browser window does not open
-        firefox_options.add_argument("--no-sandbox")
-        firefox_options.add_argument("--disable-dev-shm-usage")
-        self.driver = webdriver.Firefox(options=firefox_options)
+        options = Options()
+        options.add_argument("--headless")
+        self.driver = webdriver.Firefox(options=options)
 
     def test_h5_tag_content(self):
         driver = self.driver
-        driver.get("http://10.48.229.158")  # Replace with your target website
+        driver.get("http://10.48.229.158:8081")  # ✅ 确认这里地址正确！
         
-        # Locate the <h5> tag and get its text
-        h5_text = driver.find_element(By.TAG_NAME, "h5").text
+        # 等待 h5 元素出现
+        h5_element = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.TAG_NAME, "h5"))
+        )
+        h5_text = h5_element.text
         
-        # Assert that the text of the <h5> tag is "Lab 3-7 Works!"
-        self.assertEqual("Lab 3-7 Works!", h5_text, "The <h5> tag does not contain the text 'Lab 3-7 Works!'")
+        self.assertEqual(h5_text, "Lab 3-7 Works!", "The <h5> tag does not contain the expected text")
 
     def tearDown(self):
         self.driver.quit()
